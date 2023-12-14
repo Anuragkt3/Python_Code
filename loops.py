2e3e4ed204ba79f0ae47386d16b68da22029f96a
Anurag <anuragkt3@gmail.com>
2023-12-14 16:16:58 +0530

    loop

--- loops.py
+++ loops.py
@@ -59,4 +59,37 @@ prod = 1
 while(i>0):
     prod = prod*(i%10)
     i = i//10
-print("The product is",prod) '''
+print("The product is",prod) 
+
+
+# Check wether a number is Armstrong or not
+i = int(input("enter the number"))
+sum = 0
+ori = i
+while(i>0):
+    sum = sum + (i%10)*(i%10)*(i%10)
+    i = i//10
+if ori==sum:
+    print("Armstrong Number")
+else:
+    print("Not an Armstrong Number")        '''
+
+# Find sum of even and product of odd number
+
+i = int(input("Enter the number: "))
+sum = 0
+prod = 1
+while(i>0):
+    d = i%10
+    if d%2 == 0:
+        sum = sum+d
+    else:
+        prod = prod*d
+    i = i//10
+print("The sum is",sum,"The product is",prod)                        
+
+
+
+
+
+
